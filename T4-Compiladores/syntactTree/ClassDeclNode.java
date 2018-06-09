63c56e563dd698caa2a7974188be843c8de7b632
adria <adria@DESKTOP-VHOMPTS>
2018-06-08 23:25:11 -0300

    Atualização ClassDeclNode

--- T4-Compiladores/syntactTree/ClassDeclNode.java
+++ T4-Compiladores/syntactTree/ClassDeclNode.java
@@ -5,13 +5,12 @@ package syntactTree;
 import parser.Token;
 
 public class ClassDeclNode extends GeneralNode {
-	
+
 	public Token name;
 	public Token supername;
 	public ClassBodyNode body;
-	
-	public ClassDeclNode (Token t1, Token t2, Token t3, ClassBodyNode c)
-	{
+
+	public ClassDeclNode(Token t1, Token t2, Token t3, ClassBodyNode c) {
 		super(t1);
 		name = t2;
 		supername = t3;
