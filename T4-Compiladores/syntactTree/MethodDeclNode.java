cf3186bb28fba0ecfffc367fa8d07ae9c84d8906
adria <adria@DESKTOP-VHOMPTS>
2018-06-09 11:44:52 -0300

    MethodDeclNode atualizado com o livro.

--- T4-Compiladores/syntactTree/MethodDeclNode.java
+++ T4-Compiladores/syntactTree/MethodDeclNode.java
@@ -1,19 +1,18 @@
-//Programa 6.15
+// Programa 6.15
 
 package syntactTree;
 
 import parser.*;
 
-
 public class MethodDeclNode extends GeneralNode {
-    public int dim;
-    public Token name;
-    public MethodBodyNode body;
+	public int dim;
+	public Token name;
+	public MethodBodyNode body;
 
-    public MethodDeclNode(Token t, int k, Token t2, MethodBodyNode m) {
-        super(t);
-        dim = k;
-        name = t2;
-        body = m;
-    }
+	public MethodDeclNode(Token t, int k, Token t2, MethodBodyNode m) {
+		super(t);
+		dim = k;
+		name = t2;
+		body = m;
+	}
 }
