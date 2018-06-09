ff77c67d39fe269c69313a69b5d824c7fd21d0dc
Adriano Lima <adrianolslima@gmail.com>
2018-06-09 12:45:17 -0300

    BlockNode atualizado com o livro.

--- T4-Compiladores/syntactTree/BlockNode.java
+++ T4-Compiladores/syntactTree/BlockNode.java
@@ -1,15 +1,15 @@
-//Programa 6.22
+// Programa 6.22
 
 package syntactTree;
 
 import parser.*;
 
-
 public class BlockNode extends StatementNode {
-    public ListNode stats;
 
-    public BlockNode(Token t, ListNode l) {
-        super(t);
-        stats = l;
-    }
+	public ListNode stats;
+
+	public BlockNode(Token t, ListNode l) {
+		super(t);
+		stats = l;
+	}
 }
