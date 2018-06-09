d879845ac8d4237c07eb807ffd88e61f7c9a8d66
adria <adria@DESKTOP-VHOMPTS>
2018-06-09 11:50:07 -0300

    MethodBodyNode atualizado com o livro.

--- T4-Compiladores/syntactTree/MethodBodyNode.java
+++ T4-Compiladores/syntactTree/MethodBodyNode.java
@@ -1,17 +1,16 @@
-//Programa 6.17
+// Programa 6.17
 
 package syntactTree;
 
 import parser.*;
 
-
 public class MethodBodyNode extends GeneralNode {
-    public ListNode param;
-    public StatementNode stat;
+	public ListNode param;
+	public StatementNode stat;
 
-    public MethodBodyNode(Token t, ListNode l, StatementNode s) {
-        super(t);
-        param = l;
-        stat = s;
-    }
+	public MethodBodyNode(Token t, ListNode l, StatementNode s) {
+		super(t);
+		param = l;
+		stat = s;
+	}
 }
