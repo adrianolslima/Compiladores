24aa779f4b4d97719460f780cfc103f51fffa2a3
Adriano Lima <adrianolslima@gmail.com>
2018-06-09 15:19:45 -0300

    UnaryNode atualizadoo com o livro..

--- T4-Compiladores/syntactTree/UnaryNode.java
+++ T4-Compiladores/syntactTree/UnaryNode.java
@@ -1,15 +1,15 @@
-//Programa 6.51
+// Programa 6.51
 
 package syntactTree;
 
 import parser.*;
 
-
 public class UnaryNode extends ExpreNode {
-    public ExpreNode expr;
 
-    public UnaryNode(Token t, ExpreNode e) {
-        super(t);
-        expr = e;
-    }
+	public ExpreNode expr;
+
+	public UnaryNode(Token t, ExpreNode e) {
+		super(t);
+		expr = e;
+	}
 }
