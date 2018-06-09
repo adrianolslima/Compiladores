6c53b72cb66997be4380e77dd83293f231c0302c
Adriano Lima <adrianolslima@gmail.com>
2018-06-09 12:46:48 -0300

    BreakNode atualizado com o livro.

--- T4-Compiladores/syntactTree/BreakNode.java
+++ T4-Compiladores/syntactTree/BreakNode.java
@@ -1,12 +1,12 @@
-//Programa 6.23
+// Programa 6.23
 
 package syntactTree;
 
 import parser.*;
 
-
 public class BreakNode extends StatementNode {
-    public BreakNode(Token t) {
-        super(t);
-    }
+	
+	public BreakNode(Token t) {
+		super(t);
+	}
 }
