f9bc24e528f18068807f1fb431d95d08b7aa51f3
Adriano Lima <adrianolslima@gmail.com>
2018-06-09 14:52:24 -0300

    ForNode atualizado com o livro.

--- T4-Compiladores/syntactTree/ForNode.java
+++ T4-Compiladores/syntactTree/ForNode.java
@@ -1,22 +1,21 @@
-//Programa 6.43
+// Programa 6.43
 
 package syntactTree;
 
 import parser.*;
 
-
 public class ForNode extends StatementNode {
-    public AtribNode init;
-    public AtribNode incr;
-    public StatementNode stat;
-    public ExpreNode expr;
 
-    public ForNode(Token t, ExpreNode e, AtribNode s1, AtribNode s2,
-        StatementNode s3) {
-        super(t);
-        expr = e;
-        init = s1;
-        incr = s2;
-        stat = s3;
-    }
+	public AtribNode init;
+	public AtribNode incr;
+	public StatementNode stat;
+	public ExpreNode expr;
+
+	public ForNode(Token t, ExpreNode e, AtribNode s1, AtribNode s2, StatementNode s3) {
+		super(t);
+		expr = e;
+		init = s1;
+		incr = s2;
+		stat = s3;
+	}
 }
