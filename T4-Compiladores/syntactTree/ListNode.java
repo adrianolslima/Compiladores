6ffaf34ca9d25209250a0e78cd3357bc9e73b7f9
adria <adria@DESKTOP-VHOMPTS>
2018-06-08 23:25:24 -0300

    Atualização ListNode

--- T4-Compiladores/syntactTree/ListNode.java
+++ T4-Compiladores/syntactTree/ListNode.java
@@ -3,31 +3,28 @@
 package syntactTree;
 
 public class ListNode extends GeneralNode {
-	
+
 	public GeneralNode node;
 	public ListNode next;
-	
-	public ListNode (GeneralNode t2)
-	{
+
+	public ListNode(GeneralNode t2) {
 		super(t2.position);
 		node = t2;
 		next = null;
 	}
-	
-	public ListNode (GeneralNode t2, ListNode l)
-	{
+
+	public ListNode(GeneralNode t2, ListNode l) {
 		super(t2.position);
 		node = t2;
 		next = l;
 	}
-	
-	public void add (GeneralNode t2)
-	{
+
+	public void add(GeneralNode t2) {
 		if (next == null) // Verifica se eh ultimo
 			next = new ListNode(t2); // Insere no final
-		else 
+		else
 			next.add(t2); // insere após o próximo
-		
+
 	}
 
 }
