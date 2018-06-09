4ce5a05deebfd1325bc616d7a49a2eab7d92bac5
Adriano Lima <adrianolslima@gmail.com>
2018-06-09 12:48:53 -0300

    AtribNode atualizado com o livro.

--- T4-Compiladores/syntactTree/AtribNode.java
+++ T4-Compiladores/syntactTree/AtribNode.java
@@ -1,17 +1,16 @@
-//Programa 6.25
+// Programa 6.25
 
 package syntactTree;
 
 import parser.*;
 
-
 public class AtribNode extends StatementNode {
-    public ExpreNode expr1, expr2;
 
+	public ExpreNode expr1, expr2;
 
-    public AtribNode(Token t, ExpreNode e1, ExpreNode e2) {
-        super(t);
-        expr1 = e1;
-        expr2 = e2;
-    }
+	public AtribNode(Token t, ExpreNode e1, ExpreNode e2) {
+		super(t);
+		expr1 = e1;
+		expr2 = e2;
+	}
 }
